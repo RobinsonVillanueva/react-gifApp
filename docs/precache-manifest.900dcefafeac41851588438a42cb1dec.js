d0258cccbc5f6032182c9afd610b754a895fe916
Robinson Villanueva <robinson.villanueva95@gmail.com>
2020-08-05 17:22:33 -0400

    Docs agregado

---
+++ docs/precache-manifest.900dcefafeac41851588438a42cb1dec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ededf7a677c77514661ea0e16de95b53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbafbd21234efc8ae20e",
+    "url": "/static/css/main.8f6cc4ac.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbafbd21234efc8ae20e",
+    "url": "/static/js/main.5127eb95.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
